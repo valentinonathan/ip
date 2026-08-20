0ee6d0fa83de4b2aac17c7bfb8abc0c6e6781a7c
Valentino Nathan <nathan070907@gmail.com>
2026-08-20 17:05:03 +0800

    Add greeting

--- src/main/java/Oreo.java
+++ src/main/java/Oreo.java
@@ -27,5 +27,20 @@ public class Oreo {
                 ____________________________________________________________""";
         System.out.println(banner);
         System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (command.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
